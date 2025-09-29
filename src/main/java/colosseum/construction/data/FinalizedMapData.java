1e16153608061e865af1b8416a0c9a02cbacf1ba
NorthRealm <155140859+NorthRealm@users.noreply.github.com>
2025-09-29 13:22:12 +0800

    QoS

--- src/main/java/colosseum/construction/data/FinalizedMapData.java
+++ src/main/java/colosseum/construction/data/FinalizedMapData.java
@@ -4,45 +4,38 @@ import colosseum.utility.MapData;
 import colosseum.utility.arcade.GameType;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
 import java.util.UUID;
 
+@AllArgsConstructor
 public final class FinalizedMapData implements MapData {
-    @Getter
-    private final boolean live;
-    private final ImmutableMap<String, Vector> warps;
-    private final ImmutableSet<UUID> adminList;
-    @Getter
-    private final GameType mapGameType;
     @Getter
     private final String mapName;
     @Getter
     private final String mapCreator;
+    @Getter
+    private final GameType mapGameType;
+    private final ImmutableMap<String, Vector> warps;
+    private final ImmutableSet<UUID> adminList;
+    @Getter
+    private final boolean live;
 
-    public FinalizedMapData(MutableMapData mapData) {
-        this.live = mapData.isLive();
-        this.warps = mapData.warps();
-        this.adminList = mapData.adminList();
-        this.mapGameType = mapData.getMapGameType();
-        this.mapName = mapData.getMapName();
-        this.mapCreator = mapData.getMapCreator();
+    public FinalizedMapData(MapData mapData) {
+        this(mapData.getMapName(), mapData.getMapCreator(), mapData.getMapGameType(), mapData.warps(), mapData.adminList(), mapData.isLive());
     }
 
     @Override
     public ImmutableMap<String, Vector> warps() {
-        ImmutableMap.Builder<String, Vector> builder = ImmutableMap.builder();
-        warps.forEach((s, l) -> builder.put(s, l.clone()));
-        return builder.build();
+        return warps;
     }
 
     @Override
     public ImmutableSet<UUID> adminList() {
-        ImmutableSet.Builder<UUID> builder = ImmutableSet.builder();
-        adminList.forEach(v -> builder.add(UUID.fromString(v.toString())));
-        return builder.build();
+        return adminList;
     }
 
     @Override
